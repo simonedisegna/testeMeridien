95f769721db3068caafa33d79aa32285b525171a
Disegna <Disegna@DESKTOP-CK26KF7>
2021-06-24 10:58:09 -0300

    Incluindo informações na classe indicando o erro.

--- controller/classe.php
+++ controller/classe.php
@@ -21,11 +21,13 @@
         }
     }
     
-    $carro = new Carro;
-    $carro->marca = "Kombi";
-    $carro->cor = "branca";
+  /* Por questões de boa prática, não devemos incluir linha de código após a finalização da classe, e não teria lógica.
+   * correto é chamar a classe por require_once e assim estânciar o mesmo. 
+   * $carro = new Carro;
+    $carro->marca = "Kombi"; //Como a váriavel é chamada via set e get o correto seria $carro->getMarca, mas conforme a classe acima não foi indicado como váriável.
+    $carro->cor = "branca"; // conforme a classe acima, o correto é chamar $carro->setCor;
     
-    echo $carro->marca;
-    echo $carro->cor;
+    echo $carro->marca; //vai da erro pois seria getMarca ele não incluido na classe acima
+    echo $carro->cor;*/ //vai da erro $carro->getCor;
     
 ?>

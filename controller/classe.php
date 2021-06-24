41803b987759e8ebc2bc3df9fcff8949c2014776
Disegna <Disegna@DESKTOP-CK26KF7>
2021-06-24 14:27:16 -0300

    Verificado todos os detalhes e ajustes finais para aparecer as informações da classe, conforme boa prática, estanciei a classe no index.php e assim fiz a chamada corretamente seguindo os passo de seguraça da OO.

--- controller/classe.php
+++ controller/classe.php
@@ -1,33 +1,36 @@
 <?php
-	
-    class Carro
-    {
-        public $marca;
-        private $cor;
-        
-        private function __construct()
-        {
-            echo "instanciando a classe";
-        }
-        
-        public function setCor($cor)
-        {
-            $this->cor = $cor;
-        }
-        
-        public function getCor()
-        {
-            return $cor; // faltou o $this return $this->cor;
-        }
+
+class Carro {
+
+    public $marca;
+    private $cor;
+
+    //  private function __construct()//o construtor sendo privado, a página não irá permitir as visualizações das variaveis o ideal ele fique publico
+    function __construct() {
+        // echo "instanciando a classe"; com echo dará um erro correto print
+        print "<b>instanciando a classe</b>";       
     }
-    
-  /* Por questões de boa prática, não devemos incluir linha de código após a finalização da classe.
-   * correto é chamar a classe por require_once e assim estânciar o mesmo. 
-   * $carro = new Carro;
-    $carro->marca = "Kombi"; //váriável pública esta correta
-    $carro->cor = "branca"; // a váriavel é privada por esse motivo deve ser utilizada pela chamada set e get, o correto seria $carro->setCor;
-    
-    echo $carro->marca; 
-    echo $carro->cor;*/ //vai da erro $carro->getCor;
-    
+
+    public function setCor($cor) {
+        $this->cor = $cor;
+    }
+
+    public function getCor() {
+        //return $cor; // faltou o $this return $this->cor;
+        return $this->cor; //forma correta
+    }
+
+}
+
+/* Por questões de boa prática, não devemos incluir linha de código após a finalização da classe.
+ * correto é chamar a classe por require_once e assim estânciar o mesmo. 
+  $carro = new Carro;
+  $carro->marca = "Kombi"; //váriável pública esta correta
+  // $carro->cor = "branca"; // a váriavel é privada por esse motivo deve ser utilizada pela chamada set e get, o correto seria $carro->setCor;
+  $carro->setCor("branca");
+  echo $carro->marca;
+  // echo $carro->cor; //vai da erro $carro->getCor;
+  echo $carro->getCor;//forma correta
+
+  //OBS: antes de rodar já havia indentificados esses defeitos dos set Get, o construtor não se se seria erro, mas ele provado trava toda a classe, assim não recomendado, já que a váriavel já privada */
 ?>
